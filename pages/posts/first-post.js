92805c4206c902551ab0bb1a5737f7020dc474ba
Mountian Driver <lsfreigenberg@outlook.com>
2022-12-06 18:41:23 -0700

    Complete Assest, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,23 @@
+import Script from "next/script";
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
-    return(
-        <>
-        <h1>First Post</h1>;
-        <h2>
-            <Link href='/'>Back to home</Link>
-        </h2>
-        </>
-    ); 
-  }
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() => console.log('script loaded correctly, window.FB has been poulated')}
+            />
+            <h1>First Post</h1>
+            <h2>
+                <Link href='/'>Back to home</Link>
+            </h2>
+        </Layout>
+    );
+}
