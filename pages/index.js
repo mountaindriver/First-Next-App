678569c6f8207c70cea8af8f5073520a41f3d58c
Mountian Driver <lsfreigenberg@outlook.com>
2022-12-07 10:56:39 -0700

    Complete Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,43 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I am a Full Stack Developer. You can contact me at <a href='https://www.linkedin.com/in/lucas-freigenberg-539338134/'>LinkedIn</a></p>
+        <p>Hello, I am a Full Stack Developer. You can contact me at my <a href='https://www.linkedin.com/in/lucas-freigenberg-539338134/'>LinkedIn</a></p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a sample website - I built based off of <a href="https://nextjs.org/learn"> Next.js's tutorial</a>.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMD} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
