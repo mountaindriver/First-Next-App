045d0cd68e92f664ae1a0d6f8725e070729328aa
Mountian Driver <lsfreigenberg@outlook.com>
2022-12-07 12:31:26 -0700

    Complete Dynamic Routes

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+
+// pages/404.js
+export default function Custom404() {
+    return (
+        <Layout>
+            <section>
+                <h1>404 - Page Not Found</h1>
+            </section>
+        </Layout>
+    )
+}
